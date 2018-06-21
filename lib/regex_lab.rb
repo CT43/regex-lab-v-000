735496fec9e5daea741767063c36782d96864c62
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-21 16:29:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,26 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]\w+/) ? true:false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[.?!]$/) ? true:false
 end
 
 def valid_phone_number?(phone)
-
+ numbers = phone.each do |i|
+    splitnum = i.split("")
+    if splitnum.length == 10
+      return true
+     else
+     return false
+     end
+ end
 end
